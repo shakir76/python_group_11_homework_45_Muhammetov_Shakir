84764d2a30492de31042eda5487347d6742281e7
shakir <shakirmuhammetov@gmail.com>
2022-06-24 16:53:06 +0600

    Создал функцию показа статьи

--- todolist/views.py
+++ todolist/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from todolist.models import Task
+
 
 def index(request):
-    return render(request, "index.html")
+    task = Task.objects.order_by("-created_at")
+    context = {"tasks": task}
+    return render(request, "index.html", context)
